7cc5d2f99f4b63d7df1d9af4937a73aa2e4a3cc1
octavian <gogol.logog@hotmail.it>
2018-09-26 15:23:16 +0200

    Included personal header files. Program logic to be rebuilt

---
+++ Manager/Manager.c
@@ -0,0 +1,237 @@
+#include <syslog.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+#include "../useful.h"
+
+#define CHUNK 16
+
+/* Hold process name and PID */
+typedef struct pids {
+	char name[CHUNK];
+	int pid;
+	struct pids * next;
+} pids_t;
+
+/* Check if processes are running */
+int checker(pids_t **head, char *dirname) {
+	if (kill((*head)->pid, 0) == 0) {
+		/* process is running or a zombie */
+		return 0;
+	}
+	else if (errno == ESRCH) {
+		/* no process with the given pid is running */
+		int i, num = 0;
+		size_t len;
+		char *res = NULL;
+		FILE *fp;
+		char *path = malloc(strlen(dirname) + strlen((*head)->name) + 2);
+
+		sprintf(path, "%s/%s", dirname, (*head)->name);
+		fp = popen(path, "r");
+		free(path);
+		if (fp == NULL) {
+			/* File not found */
+			return 2;
+		}
+		while ((num = reader(fp, &res, CHUNK))) {
+			if (pclose(fp) / 256 == 127) {
+				/* File not found */
+				return 2;
+			}
+			return (num*(-1)) - 1;
+		}
+		if (pclose(fp) / 256 == 127) {
+			/* File not found */
+			return 2;
+		}
+		res[strcspn(res, "\n")] = 0;
+		len = strlen(res);
+		num = 0;
+		for (i = 0; (unsigned int)i < len; i++) {
+			num = num * 10 + (res[i] - '0');
+		}
+		(*head)->pid = num;
+		free(res);
+		return 1;
+	}
+	else {
+		/* some other error... use perror("...") or strerror(errno) to report */
+		return -1;
+	}
+}
+
+/* First function to be called, finds scripts and starts them */
+int starter(pids_t **head, char *dirname) {
+	DIR *pDir;
+	struct dirent *pDirent;
+	FILE *fp;
+	char *res, *path;
+	pids_t *current;
+	int len, i, num;
+
+	/* Open the given directory */
+	pDir = opendir(dirname);
+	if (pDir == NULL) {
+		syslog(LOG_ERR, "Cannot open directory \"%s\"\n", dirname);
+		closedir(pDir);
+		return 0;
+	}
+
+	while ((pDirent = readdir(pDir)) != NULL) {
+		if (pDirent->d_name[0] == '.') continue;
+
+		path = malloc(strlen(dirname) + strlen(pDirent->d_name) + 2);
+		sprintf(path, "%s/%s", dirname, pDirent->d_name);
+
+		fp = popen(path, "r");
+		free(path);
+		if (fp == NULL) {
+			syslog(LOG_ERR, "File not found");
+			continue;
+		}
+		while ((num = reader(fp, &res, CHUNK))) {
+			syslog(LOG_ERR, "Error reading output: %d", num);
+			continue;
+		}
+		if (pclose(fp) / 256 == 127) {
+			syslog(LOG_ERR, "File not found");
+			continue;
+		}
+
+		res[strcspn(res, "\n")] = 0;
+		len = strlen(res);
+		num = 0;
+		for (i = 0; i < len; i++) {
+			num = num * 10 + (res[i] - '0');
+		}
+		free(res);
+
+		if (*head == NULL) {
+			*head = malloc(sizeof(pids_t));
+			if (*head == NULL) {
+				syslog(LOG_ERR, "Cannot create process list");
+				closedir(pDir);
+				return 0;
+			}
+			current = *head;
+		}
+		else {
+			current->next = malloc(sizeof(pids_t));
+			if (current->next == NULL) {
+				syslog(LOG_ERR, "Cannot create process entry");
+				continue;
+			}
+			current = current->next;
+		}
+
+		snprintf(current->name, CHUNK, "%s", pDirent->d_name);
+		current->pid = num;
+		current->next = NULL;
+
+	}
+	free(pDirent);
+	closedir(pDir);
+	return 1;
+}
+
+/* Check if a new scripts exists in the directory */
+int updater(pids_t **head, char *dirname) {
+	DIR *pDir;
+	struct dirent *pDirent;
+	pids_t *current;
+
+	pDir = opendir(dirname);
+	while ((pDirent = readdir(pDir)) != NULL) {
+		if (pDirent->d_name[0] == '.') continue;
+		
+		current = *head;
+		while (current != NULL) {
+			if (strcmp(pDirent->d_name, current->name) == 0) break;
+
+			else if (current->next == NULL) {
+				current->next = malloc(sizeof(pids_t));
+				if (current->next == NULL) {
+					syslog(LOG_ERR, "Cannot create process entry");
+					continue;
+				}
+				current = current->next;
+				snprintf(current->name, CHUNK, "%s", pDirent->d_name);
+				current->pid = 32769;
+				current->next = NULL;
+				syslog(LOG_WARNING, "Added process %s", current->name);
+			}
+			current = current->next;
+		}
+
+	}
+	free(pDirent);
+	closedir(pDir);
+	return 1;
+}
+
+int main() {
+	int proc_status;
+	pids_t *removed, *current, *head = NULL;
+	char directory[] = "startup_scripts";
+	openlog("Manager", LOG_PERROR | LOG_PID | LOG_NDELAY, 0);
+	setlogmask(LOG_UPTO(LOG_WARNING));
+
+	/* Obtain initial list of processes and start them */
+	if (starter(&head, directory) == 0) {
+		fprintf(stderr, "Error starting scripts\n");
+		exit(EXIT_FAILURE);
+	}
+
+	while (1) { // Keep this program alive
+		current = head;
+		/* Check if list changed and if processes are still running. If they stopped, restart them */
+		updater(&head, directory);
+		while (current != NULL) {
+			syslog(LOG_NOTICE, "Testing %s with PID %d... ", current->name, current->pid);
+
+			proc_status = checker(&current, directory);
+			if (proc_status == 0) {
+				syslog(LOG_NOTICE, "%s running OK", current->name);
+			}
+			else if (proc_status == 1) {
+				syslog(LOG_WARNING, "%s restarted. PID %d\n", current->name, current->pid);
+			}
+			else if (proc_status == 2) {
+				syslog(LOG_ERR, "%s not running, script not found", current->name);
+				if (current == head) {
+					head = head->next;
+					free(current);
+					syslog(LOG_WARNING, "Removed");
+					current = head;
+				}
+				else {
+					removed = current;
+					current = head;
+					while (current->next != removed) {
+						current = current->next;
+					}
+					current->next = current->next->next;
+					free(removed);
+					syslog(LOG_WARNING, "Removed");
+					current = head;
+				}
+
+			}
+			else if (proc_status == -1) {
+				syslog(LOG_ERR, "Error: %s", strerror(errno));
+			}
+			else if (proc_status < -1) {
+				syslog(LOG_WARNING, "Error parsing output from %s", current->name);
+			}
+			current = current->next;
+			sleep(1);
+		}
+		sleep(30);
+	}
+	return 0;
+}
